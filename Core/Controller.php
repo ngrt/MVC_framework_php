292da679c1c2fa0cacc038b6efd49648e007ae35
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-11-30 11:11:50 +0100

    layout set up

--- Core/Controller.php
+++ Core/Controller.php
@@ -2,7 +2,7 @@
     class Controller
     {
         var $vars = [];
-        var layout = 'default';
+        var $layout = "default";
 
         function set($d)
         {
@@ -12,7 +12,18 @@
         function render($filename)
         {
             extract($this->vars);
+            ob_start();
             require(ROOT . "Views/" . get_class($this) . '/' . $filename . '.php');
+            $content_for_layout = ob_get_clean();
+
+            if ($this->layout == false)
+            {
+                $content_for_layout;
+            }
+            else
+            {
+                require(ROOT . "Views/Layouts/" . $this->layout . '.php');
+            }
         }
     }
 ?>
--- Views/Layouts/default.php
+++ Views/Layouts/default.php
@@ -1,1 +1,5 @@
 <h1>Bienvenue sur mon site de ouuuuuuuf</h1>
+
+<?php echo $content_for_layout;?>
+
+<p>Mon footer</p>
